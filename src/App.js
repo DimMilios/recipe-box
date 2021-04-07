ed8242fd108424cf0e2e6ace1732d082ef3adb12
DimMilios <elles1992@yahoo.gr>
2021-04-07 13:04:48 +0300

    move new recipe form to its own file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { recipes as recipeData } from './recipes';
 import { RecipeList } from './components/RecipeList';
+import { AddRecipe } from './components/AddRecipe';
 
 function App() {
   const [recipes, setRecipes] = useState([]);
@@ -55,50 +56,6 @@ function App() {
         handleDirectionsChange={handleDirectionsChange}
       />
     </div>
-  );
-}
-
-function AddRecipe({
-  name,
-  ingredients,
-  directions,
-  handleSubmit,
-  handleNameChange,
-  handleIngredientsChange,
-  handleDirectionsChange,
-}) {
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Recipe name</label>
-        <input
-          name="name"
-          type="text"
-          value={name}
-          onChange={handleNameChange}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="ingredients">Ingredients</label>
-        <textarea
-          name="ingredients"
-          value={ingredients}
-          onChange={handleIngredientsChange}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="directions">Directions</label>
-        <textarea
-          name="directions"
-          value={directions}
-          onChange={handleDirectionsChange}
-        />
-      </div>
-
-      <button>Add Recipe</button>
-    </form>
   );
 }
 
---
+++ src/components/AddRecipe.js
@@ -0,0 +1,43 @@
+export function AddRecipe({
+  name,
+  ingredients,
+  directions,
+  handleSubmit,
+  handleNameChange,
+  handleIngredientsChange,
+  handleDirectionsChange,
+}) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Recipe name</label>
+        <input
+          name="name"
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="ingredients">Ingredients</label>
+        <textarea
+          name="ingredients"
+          value={ingredients}
+          onChange={handleIngredientsChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="directions">Directions</label>
+        <textarea
+          name="directions"
+          value={directions}
+          onChange={handleDirectionsChange}
+        />
+      </div>
+
+      <button>Add Recipe</button>
+    </form>
+  );
+}
